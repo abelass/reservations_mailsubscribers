82f597893833a8b6b28c91550331f979fd35f715
Rainer Müller <rainer@websimple.be>
2014-01-03 15:22:55 +0100

    import initial

---
+++ lang/paquet-reservations_mailsubscribers_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'reservations_mailsubscribers_description' => 'Lien entre le plugin réservations et mailsubscribers',
+	'reservations_mailsubscribers_nom' => 'Réservation 2 Mailsubscribers',
+	'reservations_mailsubscribers_slogan' => 'Lien entre le plugin réservations et mailsubscribers',
+);
+
+?>
---
+++ reservations_mailsubscribers_pipelines.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Utilisations de pipelines par Réservation 2 Mailsubscribers
+ *
+ * @plugin     Réservation 2 Mailsubscribers
+ * @copyright  2014
+ * @author     Rainer
+ * @licence    GNU/GPL
+ * @package    SPIP\Reservations_mailsubscribers\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
+
+
+
+
+?>
