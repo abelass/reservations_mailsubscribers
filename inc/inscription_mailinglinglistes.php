712251b3058c61f086e079cc18d852db9385d50d
Rainer <rainer@websimple.be>
2015-09-26 18:33:13 +0200

    recuperer les valeurs à disposition

--- inc/inscription_mailinglinglistes.php
+++ inc/inscription_mailinglinglistes.php
@@ -3,9 +3,12 @@
 if (!defined('_ECRIRE_INC_VERSION'))
   return;
 
-function inc_inscription_mailinglinglistes_dist($email) {
+function inc_inscription_mailinglinglistes_dist($set=array()) {
   $listes = _request('listes') ? _request('listes') : array();
-
+  $email =  isset($set['email']) ? $set['email'] : (_request('email') ? _request('email') : session_get('email'));
+  $nom = isset($set['nom']) ? $set['nom'] : (_request('nom') ? _request('nom') : session_get('nom'));
+  $lang =  isset($set['lang']) ? $set['lang'] : (_request('lang') ? _request('lang') : $GLOBALS['spip_lang']);
+  
   //Les liste cachés ne sontpas pris en compte si l'abonné est désinscrit ou à la poubelle
   if (!$id_mailsubscriber = sql_getfetsel('id_mailsubscriber', 'spip_mailsubscribers', 'email=' . sql_quote($email) . ' AND statut IN ("refuse","poubelle")') OR count($listes) > 0) {
     if ($listes_caches = _request('listes_caches')) {
@@ -18,11 +21,11 @@ function inc_inscription_mailinglinglistes_dist($email) {
   if (count($listes) > 0) {
     $options = array(
       'lang' => $GLOBALS['spip_lang'],
-      'nom' => _request('nom'),
+      'nom' => $set['email'],
       'listes' => $listes
     );
     $newsletter_subscribe = charger_fonction("subscribe", "newsletter");
-    $newsletter_subscribe($email, $options);
+    $newsletter_subscribe($set['email'], $options);
   }
   return;
 }
